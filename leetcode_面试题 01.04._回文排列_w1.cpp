e6699debaa51dc27e0e6b73704d5db9cc0a3f675
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-04-29 15:27:26 +0800

    Add files via upload

---
+++ leetcode_面试题 01.04._回文排列_w1.cpp
@@ -0,0 +1,86 @@
+﻿/* ------------------------------------------------------------------|
+给定一个字符串，编写一个函数判定其是否为某个回文串的排列之一。
+
+回文串是指正反两个方向都一样的单词或短语。排列是指字母的重新排列。
+
+回文串不一定是字典当中的单词。
+
+ 
+
+示例1：
+
+输入："tactcoa"
+输出：true（排列有"tacocat"、"atcocta"，等等）
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/palindrome-permutation-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：6.1 MB, 在所有 C++ 提交中击败了61.99%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+bool canPermutePalindrome(string s) {
+    bool nums[128] = { 0 };
+    for(char &c : s){
+        nums[c] = 1 - nums[c];
+    }
+    bool cnt = 0;
+    for (bool b : nums) {
+        if (cnt && b) {
+            return false;
+        }
+        else if (b) {
+            cnt = 1;
+        }
+    }
+    return true;
+}
+
+int main() {
+    string s = "tactcoa";
+    cout << canPermutePalindrome(s);
+}
